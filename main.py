46f7961e812979bd1c23032c77065a72a190d890
25mmaynard <25mmaynard>
2023-02-08 21:03:31 -0500

    fixed config handeling bugs; apriltags detect working fully

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
-def file_write(file, parser, threads, decimate, blur, refine, sharpen, atdebug, decisionmargin):
+def file_write(file, threads, decimate, blur, refine, sharpen, atdebug, decisionmargin):
 
-    parser['VISION'] = {THREADS_TOPIC_NAME: str(threads),
-                     DECIMATE_TOPIC_NAME: str(decimate),
-                     BLUR_TOPIC_NAME : str(blur),
-                     REFINE_EDGES_TOPIC_NAME : str(refine),
-                     SHARPENING_TOPIC_NAME : str(sharpen),
-                     APRILTAG_DEBUG_MODE_TOPIC_NAME : str(atdebug),
-                     DECISION_MARGIN_TOPIC_NAME : str(decisionmargin),
-                     CONFIG_FILE_TOPIC_NAME : str(file)
-                     }
+    parser = configparser.ConfigParser()
+
+    parser.add_section('VISION')
+    parser.set('VISION', THREADS_TOPIC_NAME, str(threads))
+    parser.set('VISION', BLUR_TOPIC_NAME, str(blur))
+    parser.set('VISION', REFINE_EDGES_TOPIC_NAME, str(refine))
+    parser.set('VISION', SHARPENING_TOPIC_NAME, str(sharpen))
+    parser.set('VISION', APRILTAG_DEBUG_MODE_TOPIC_NAME, str(atdebug))
+    parser.set('VISION', DECISION_MARGIN_TOPIC_NAME, str(decisionmargin))
+    parser.set('VISION', DECIMATE_TOPIC_NAME, str(decimate))
+    parser.set('VISION', CONFIG_FILE_TOPIC_NAME, str(file))
 
     with open(file, 'w') as config:
         parser.write(config)
@@ -17,34 +19,47 @@ def file_write(file, parser, threads, decimate, blur, refine, sharpen, atdebug, 
         container.write(file)
 
 def file_read(parser, configfile_failure_ntt):
-    container_exists = os.path.isfile('Config_file_name_holder_file')
+    container_exists = os.path.isfile('Config_file_name_holder_file') #normal file read case
     if container_exists == True:
         container = open('Config_file_name_holder_file', 'r')
         config_file = container.readline()
         container.close()
         config_exists = os.path.isfile(config_file)
         if config_exists == True:
-            config_hndl = open(config_file, 'r')
-            parser.read(config_hndl)
-            config_hndl.close()
+            parser.read(config_file)
+            configfile_failure_ntt.set(False) #if it works mark no error
     else: # re-create config and container file to default
         configfile_failure_ntt.set(True) # set error for config file
-        parser['VISION'] = {THREADS_TOPIC_NAME: str(THREADS_DEFAULT),
-                    DECIMATE_TOPIC_NAME: str(DECIMATE_DEFAULT),
-                    BLUR_TOPIC_NAME : str(BLUR_DEFAULT),
-                    REFINE_EDGES_TOPIC_NAME : str(REFINE_EDGES_DEFAULT),
-                    SHARPENING_TOPIC_NAME : str(SHARPENING_DEFAULT),
-                    APRILTAG_DEBUG_MODE_TOPIC_NAME : str(APRILTAG_DEBUG_MODE_DEFAULT),
-                    DECISION_MARGIN_TOPIC_NAME : str(DECISION_MARGIN_DEFAULT),
-                    CONFIG_FILE_TOPIC_NAME : str(CONFIG_FILE_DEFAULT)
-                    }
 
-        with open(CONFIG_FILE_DEFAULT, 'w') as config:
+        parser.add_section('VISION')
+        parser.set('VISION', THREADS_TOPIC_NAME, str(THREADS_DEFAULT))
+        parser.set('VISION', BLUR_TOPIC_NAME, str(BLUR_DEFAULT))
+        parser.set('VISION', REFINE_EDGES_TOPIC_NAME, str(REFINE_EDGES_DEFAULT))
+        parser.set('VISION', SHARPENING_TOPIC_NAME, str(SHARPENING_DEFAULT))
+        parser.set('VISION', APRILTAG_DEBUG_MODE_TOPIC_NAME, str(APRILTAG_DEBUG_MODE_DEFAULT))
+        parser.set('VISION', DECISION_MARGIN_TOPIC_NAME, str(DECISION_MARGIN_DEFAULT))
+        parser.set('VISION', DECIMATE_TOPIC_NAME, str(DECIMATE_DEFAULT))
+        parser.set('VISION', CONFIG_FILE_TOPIC_NAME, str(CONFIG_FILE_DEFAULT))
+
+        with open("/home/pi/" + CONFIG_FILE_DEFAULT, 'w') as config:
             parser.write(config)
 
-        with open('Config_file_name_holder_file', 'w') as container:
+        with open("/home/pi/" + 'Config_file_name_holder_file', 'w') as container:
             container.write(str(CONFIG_FILE_DEFAULT))
     
+def nt_update(config, threads,quadDecimate, blur, refineEdges, decodeSharpening, \
+     ATDebug, decision, configfile):
+    # sync the stuff in the file with matching values in the file
+
+    threads.set(float(config.get('VISION', THREADS_TOPIC_NAME)))
+    quadDecimate.set(float(config.get('VISION', DECIMATE_TOPIC_NAME)))
+    blur.set(float(config.get('VISION', BLUR_TOPIC_NAME)))
+    refineEdges.set(ast.literal_eval(config.get('VISION', REFINE_EDGES_TOPIC_NAME)))
+    decodeSharpening.set(float(config.get('VISION', SHARPENING_TOPIC_NAME)))
+    ATDebug.set(ast.literal_eval(config.get('VISION', APRILTAG_DEBUG_MODE_TOPIC_NAME)))
+    decision.set(float(config.get('VISION', DECISION_MARGIN_TOPIC_NAME)))
+    configfile.set(str(config.get('VISION', CONFIG_FILE_TOPIC_NAME)))
+
 def main():
     
     # start NetworkTables
@@ -57,11 +72,12 @@ def main():
  
     # Table for vision output information
     uptime_ntt = NTGetDouble(ntinst.getDoubleTopic("/Vision/Uptime"), 0, 0, -1)
-    debug_ntt = NTGetBoolean(ntinst.getBooleanTopic("/Vision/Debug Mode"), False, DEBUG_MODE_DEFAULT, DEBUG_MODE_DEFAULT)
+    #debug_ntt = NTGetBoolean(ntinst.getBooleanTopic("/Vision/Debug Mode"), False, DEBUG_MODE_DEFAULT, DEBUG_MODE_DEFAULT)
+    debug_ntt = NTGetBoolean(ntinst.getBooleanTopic("/Vision/Debug Mode"), True, True, True)
     threads_ntt = NTGetDouble(ntinst.getDoubleTopic(THREADS_TOPIC_NAME),THREADS_DEFAULT, THREADS_DEFAULT, THREADS_DEFAULT)
     quadDecimate_ntt = NTGetDouble(ntinst.getDoubleTopic(DECIMATE_TOPIC_NAME),DECIMATE_DEFAULT, DECIMATE_DEFAULT, DECIMATE_DEFAULT)
     blur_ntt = NTGetDouble(ntinst.getDoubleTopic(BLUR_TOPIC_NAME),BLUR_DEFAULT, BLUR_DEFAULT, BLUR_DEFAULT) 
-    refineEdges_ntt = NTGetDouble(ntinst.getDoubleTopic(REFINE_EDGES_TOPIC_NAME),REFINE_EDGES_DEFAULT, REFINE_EDGES_DEFAULT, REFINE_EDGES_DEFAULT) 
+    refineEdges_ntt = NTGetBoolean(ntinst.getBooleanTopic(REFINE_EDGES_TOPIC_NAME),REFINE_EDGES_DEFAULT, REFINE_EDGES_DEFAULT, REFINE_EDGES_DEFAULT) 
     decodeSharpening_ntt = NTGetDouble(ntinst.getDoubleTopic(SHARPENING_TOPIC_NAME), SHARPENING_DEFAULT, SHARPENING_DEFAULT, SHARPENING_DEFAULT)
     ATDebug_ntt = NTGetBoolean(ntinst.getBooleanTopic(APRILTAG_DEBUG_MODE_TOPIC_NAME), APRILTAG_DEBUG_MODE_DEFAULT, APRILTAG_DEBUG_MODE_DEFAULT, APRILTAG_DEBUG_MODE_DEFAULT)
     decision_margin_ntt = NTGetDouble(ntinst.getDoubleTopic(DECISION_MARGIN_TOPIC_NAME), DECISION_MARGIN_DEFAULT, DECISION_MARGIN_DEFAULT, DECISION_MARGIN_DEFAULT)
@@ -72,19 +88,22 @@ def main():
     # Wait for NetworkTables to start
     time.sleep(0.5)
     
-    # use for file
-    config = configparser.ConfigParser()
-    read_file(config, configfilefail_ntt)
-    detectorConfig.numThreads = int(config['VISION'][THREADS_TOPIC_NAME])
-    detectorConfig.quadDecimate = float(config['VISION'][DECIMATE_TOPIC_NAME])
-    detectorConfig.quadSigma = float (config['VISION'][BLUR_TOPIC_NAME])
-    detectorConfig.refineEdges = ast.literal_eval(config['VISION'][REFINE_EDGES_TOPIC_NAME])
-    detectorConfig.decodeSharpening = float(config['VISION'][SHARPENING_TOPIC_NAME])
-    detectorConfig.debug = ast.literal_eval(config['VISION'][APRILTAG_DEBUG_MODE_TOPIC_NAME])
-       
     detector = robotpy_apriltag.AprilTagDetector()
     detector.addFamily("tag16h5")
+
+    # use for file
+    config = configparser.ConfigParser()
+    file_read(config, configfilefail_ntt)
+    nt_update(config,threads_ntt, quadDecimate_ntt, blur_ntt, refineEdges_ntt, \
+        decodeSharpening_ntt, ATDebug_ntt, decision_margin_ntt, configfile_ntt)
     detectorConfig = robotpy_apriltag.AprilTagDetector.Config()
+
+    detectorConfig.numThreads = int(float(config.get('VISION', THREADS_TOPIC_NAME)))
+    detectorConfig.quadDecimate = float(config.get('VISION', DECIMATE_TOPIC_NAME))
+    detectorConfig.quadSigma = float (config.get('VISION', BLUR_TOPIC_NAME))
+    detectorConfig.refineEdges = ast.literal_eval(config.get('VISION', REFINE_EDGES_TOPIC_NAME))
+    detectorConfig.decodeSharpening = float(config.get('VISION', DECISION_MARGIN_TOPIC_NAME))
+    detectorConfig.debug = ast.literal_eval(config.get('VISION', APRILTAG_DEBUG_MODE_TOPIC_NAME))
     detector.setConfig(detectorConfig)
 
     # Capture from the first USB Camera on the system
@@ -132,14 +151,14 @@ def main():
             detectorConfig.numThreads = int(threads_ntt.get())
             detectorConfig.quadDecimate = float(quadDecimate_ntt.get())
             detectorConfig.quadSigma = float(blur_ntt.get())
-            detectorConfig.refineEdges = ast.literal_eval(refineEdges_ntt.get())
+            detectorConfig.refineEdges = refineEdges_ntt.get()
             detectorConfig.decodeSharpening = float(decodeSharpening_ntt.get())
-            detectorConfig.debug = ast.literal_eval(ATDebug_ntt.get())
+            detectorConfig.debug = ATDebug_ntt.get()
             detector.setConfig(detectorConfig)
 
         detected = detector.detect(gimg)
         for tag in detected:
-            if tag.getDecisionMargin() > decision_margin_ntt.get() and tag.getId() >= 1 and tag.getId() <= 8:
+            if tag.getDecisionMargin() > float(config.get('VISION', DECISION_MARGIN_TOPIC_NAME)) and tag.getId() >= 1 and tag.getId() <= 8:
                 if debug_ntt.get() == True:
                 
                     x0 = int(tag.getCorner(0).x)
@@ -157,8 +176,11 @@ def main():
                     cv2.putText(img, str(tag.getId()), (int(tag.getCenter().x), int(tag.getCenter().y)), cv2.FONT_HERSHEY_TRIPLEX, 1, (0, 0, 255)) # ID in cente    
         if debug_ntt.get() == True:
             outputStream.putFrame(img) # send to dashboard
-        if savefile_ntt.get() == True:
-            file_write(configfile_ntt.get(), config, threads_ntt.get(), quadDecimate_ntt.get(), blur_ntt.get(), refineEdges_ntt.get(), decodeSharpening_ntt.get(), ATDebug_ntt.get(), decision_margin_ntt.get(), configfile_ntt.get())
-            savefile_ntt.set(False)
+            if savefile_ntt.get() == True:
+                file_write(configfile_ntt.get(), threads_ntt.get(), \
+                    quadDecimate_ntt.get(), blur_ntt.get(), refineEdges_ntt.get(), \
+                    decodeSharpening_ntt.get(), ATDebug_ntt.get(), \
+                    decision_margin_ntt.get())
+                savefile_ntt.set(False)
 
 main()
