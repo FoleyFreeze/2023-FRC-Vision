ccd4d9cc889a0722b9c59ebbef62d6879a8c7fc9
roboprog <roboprog@team910.com>
2023-04-03 19:21:01 -0400

    stores HSV

---
+++ ColorConfig.py
@@ -0,0 +1,13 @@
+class ColorConfig:
+    def __init__(self) -> None:
+        pass
+    
+    def set(self, min_hue, min_sat, min_value, max_hue, max_sat, max_value ):
+        self.min_hue = min_hue
+        self.min_sat = min_sat
+        self.min_value = min_value
+        self.max_hue = max_hue
+        self.max_sat = max_sat
+        self.max_value = max_value
+        
+
